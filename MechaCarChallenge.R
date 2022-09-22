afcc835403501d95bb19fccf207a583ace317e27
Francesca Walz <francescawalz@Francescas-iMac.local>
2022-09-22 17:28:21 -0400

    Updating R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,1 +1,14 @@
 library(dplyr)
+mecha_data <- read.csv("MechaCar_mpg.csv",stringsAsFactors = F,check.names = F)
+mechaLM <- lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mecha_data)
+summary(mechaLM)
+
+suspension_data <- read.csv("Suspension_Coil.csv", stringsAsFactors = F, check.names = F)
+total_summary <- suspension_data %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = "keep")
+
+t.test(suspension_data$PSI, mu=1500)
+t.test(subset(suspension_data, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+t.test(subset(suspension_data, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+t.test(subset(suspension_data, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
+
